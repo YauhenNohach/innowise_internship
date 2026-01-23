ba317b488ba2822603f1ab206f7607958d4c5fa8
Yauhen Nohach Dev <jenaynogach1@mail.ru>
2026-01-23 14:17:33 +0300

    remove redundant

--- userservice/src/main/java/com/innowise/userservice/config/RedisConfig.java
+++ userservice/src/main/java/com/innowise/userservice/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -32,7 +33,7 @@ public class RedisConfig {
   public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
     return RedisCacheManager.builder(connectionFactory)
         .cacheDefaults(
-            org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig()
+            RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(30))
                 .disableCachingNullValues())
         .build();
